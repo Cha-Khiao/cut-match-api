08903752ba25f73a7c625a8ab788f0ad8fea0c3f
Cha-Khiao <p.prasopphol203@gmail.com>
2025-09-30 10:13:17 +0700

    add deleteNotification

--- routes/notificationRoutes.js
+++ routes/notificationRoutes.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { getNotifications, markAllAsRead } = require('../controllers/notificationController.js');
+const { 
+    getNotifications, 
+    markAllAsRead, 
+    deleteNotification // <-- import เข้ามา
+} = require('../controllers/notificationController.js');
 const { protect } = require('../middleware/authMiddleware.js');
 
 router.route('/').get(protect, getNotifications);
 router.route('/mark-read').post(protect, markAllAsRead);
+
+// --- ✨ เพิ่ม Route นี้เข้ามา ✨ ---
+router.route('/:id').delete(protect, deleteNotification);
 
 module.exports = router;
